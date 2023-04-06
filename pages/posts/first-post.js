b330668bd3178426ce2dc1e95d3402443e50bcb8
Taiki Ohara <taikiohara@example.com>
2023-04-07 00:37:13 +0900

    assets-metadata-css/global-stylesまで実施

---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back home</Link>
+      </h2>
+    </Layout>
+  );
+}
